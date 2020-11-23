9983aa0a329bef377784401f3aff06d13b8fd1d4
Cyrus Tabatabai-Yazdi <ctabatab@gmail.com>
2020-11-23 00:15:00 -0800

    use twilio api to send SMS rather than email

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests,time
+import os
 from datetime import datetime
 import smtplib
+from twilio.reset import Client
 
 
 my_email = 'calcguru2020@gmail.com'
 to_email = 'ctabatab@gmail.com'
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN']
 
 MY_LAT = 37.319309 # Your latitude
 MY_LONG = -122.029259 # Your longitude
@@ -42,11 +47,22 @@ def is_night():
 
 while True:
     if within_5_degrees() and is_night():
+        client = Client(account_sid, auth_token)
 
+        message = client.messages \
+                        .create(
+                             body="ISS is Overhead and its dark out. Look Up!",
+                             from_='+15085065496',
+                             to='+14084206801'
+                         )
+
+        print(message.status)
+        '''
         with smtp.SMTP('smtp.gmail.com',port=587) as connection:
             connection.starttls()
             connection.login(user=email,password=os.environ.get('PASSWORD'))
             connection.sendmail(from_addr=my_email,to_addrs=to_email,msg="Subject: ISS OVERHEAD\n\nLOOK UP THE ISS IS OVERHEAD")
+        '''
     time.wait(60)
 
 
